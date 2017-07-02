200d7cd7f4c00704a18961c56be01b76a1b4a53b
Elnathan Erh <elnathan.erh@gmail.com>
2017-07-02 12:43:42 +0800

    Added Create folder with Create.js and Create.css for creating task

--- todo-list-group-practice/src/components/Body/Body.js
+++ todo-list-group-practice/src/components/Body/Body.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+// Import Components
+import Create from '../Create/Create';
+
 class Body extends Component {
   render() {
     return (
       <div className="row">
 
+      <Create />
       </div>
     );
   }
