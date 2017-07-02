d2a7437c6fb4b1489849c6c609b53f57707586fb
Elnathan Erh <elnathan.erh@gmail.com>
2017-07-02 12:28:19 +0800

    added Body and Header components

---
+++ todo-list-group-practice/src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="row">
+
+      </div>
+    );
+  }
+}
+
+export default Body;
